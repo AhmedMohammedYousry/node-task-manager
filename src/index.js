ef8253e010b19ceb1df9054cf67b6b11cc9ba64b
AhmedMohammedYousry <ahmedmohammedyousry33@gmail.com>
2021-02-18 23:56:50 +0200

    Add resource update endpoint/Fix error in User model

--- src/index.js
+++ src/index.js
@@ -68,6 +68,25 @@ app.get('/users/:id', async (req, res) => {
     //     res.status(500).send()
     // })
 })
+// patch HTTP method is designed for updating an existing resource
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body) // keys will return an array of strings where each is a property on that object.
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidUpdate = updates.every((update) => allowedUpdates.includes(update))
+    if(!isValidUpdate) {
+        return res.status(400).send({ error: 'Invalid update!' })
+    }
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
 // endpoint for task creation
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
@@ -121,6 +140,25 @@ app.get('/tasks/:id', async (req, res) => {
     // })
 })
 
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const validUpdates = ['description', 'completed']
+    const isValidUpdate = updates.every((update) => validUpdates.includes(update))
+
+    if(!isValidUpdate) {
+        return res.status(400).send({ error: 'Invalid update request!' })
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(400).send()
+    }
+})
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
 })
