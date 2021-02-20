128da84259e24a5b4931a0a9d208a6745e060ef2
AhmedMohammedYousry <ahmedmohammedyousry33@gmail.com>
2021-02-20 20:20:10 +0200

    Create first middleware and replace get users route with profile route

--- src/index.js
+++ src/index.js
@@ -11,13 +11,22 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     res.status(503).send('Site is currently down. Check back soon.')
+// })
+
+
 //  when we set up this one line..
 // It's going to automatically parse incoming json to an object so we can access it in our request handlers.
 app.use(express.json())
 app.use(userRouter) // register the router
 app.use(taskRouter)
 
-
+//
+// Without middleware: new request -> run route handler
+//
+// With middleware: new request -> do something -> run route handler
+//
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
