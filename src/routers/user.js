1a20183da39484f5eb8a32b3c1ad9009d04ae573
AhmedMohammedYousry <ahmedmohammedyousry33@gmail.com>
2021-02-25 13:25:16 +0200

    Finalize avatar by normalizing image type and resolution using sharp

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const multer = require('multer')
+const sharp = require('sharp')
 
 const User = require('../models/user')
 const auth = require('../middleware/auth')
@@ -110,7 +111,8 @@ const upload = multer({
 
 // add a new endpoint for the users to upolad their profile picture
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer
+    const buffer = await sharp(req.file.buffer).resize({ width:250, height: 250 }).png().toBuffer()
+    req.user.avatar = buffer
     await req.user.save()
     res.send()
 }, (error, req, res, next) => {
@@ -118,13 +120,27 @@ router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) 
 })
 
 router.delete('/users/me/avatar', auth, async (req, res) => {
-    try {
+
     req.user.avatar = undefined
     await req.user.save()
     res.send()
-        } catch (e) {
-            res.status(500).send()
-        }
+    
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+    
+    try {
+    const user = await User.findById(req.params.id)
+
+    if(!user || !user.avatar) {
+        throw new Error()
+    }
+    res.set('Content-Type', 'image/png')
+    res.send(user.avatar)
+
+    } catch (e) {
+        res.status(404).send()
+    }
 })
 
 
